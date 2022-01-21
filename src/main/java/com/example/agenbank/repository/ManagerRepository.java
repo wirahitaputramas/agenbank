ef3c583d1efeeb3e5cc278c4348ecc499518d959
wirahitaputramas <wirahitaputramas@gmail.com>
2022-01-21 15:31:08 +0700

    AABD-1 add repo to implement stream

--- src/main/java/com/example/agenbank/repository/ManagerRepository.java
+++ src/main/java/com/example/agenbank/repository/ManagerRepository.java
@@ -2,8 +2,15 @@ package com.example.agenbank.repository;
 
 import com.example.agenbank.entity.Manager;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface ManagerRepository extends JpaRepository<Manager, Integer> {
+
+    @Query(value = "SELECT NAME FROM MANAGER M", nativeQuery = true)
+    List<String> findAllManagerName();
+
 }
